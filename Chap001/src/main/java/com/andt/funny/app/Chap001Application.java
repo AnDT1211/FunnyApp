d3b7645a2ec7a3dbe7ab34edc5a9f4462e688802
AnDT1211 <anthaido.bk@gmail.com>
2024-07-08 18:24:17 +0700

    init chap001

---
+++ Chap001/src/main/java/com/andt/funny/app/Chap001Application.java
@@ -0,0 +1,13 @@
+package com.andt.funny.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap001Application.class, args);
+	}
+
+}
